16e351290bfa87255a5f7765f673853c45316806
PabloPetit <pablopetit94@gmail.com>
2014-11-10 16:02:50 +0100

    commit

--- src/moutonsimulator/Elements/Plante.java
+++ src/moutonsimulator/Elements/Plante.java
@@ -1,18 +1,25 @@
 package moutonsimulator.Elements;
 
 import moutonsimulator.IntValMax;
+import moutonsimulator.Jeu.Case;
 
 
-public class Plante {
+public class Plante extends ElementDynamique{
     
     private IntValMax vie;
     private IntValMax age;
     private int dureePousse;
+    private Case container;
     
     public Plante(int ageMax,int vie,int dureePousse){
         this.age = new IntValMax(0, ageMax);
         this.vie = new IntValMax(vie);
         this.dureePousse = dureePousse;
+        
+    }
+
+    @Override
+    public void update() {
         
     }
     
