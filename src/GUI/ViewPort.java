5231fe88bb044c81c7c8c9addff060c21aee743d
PabloPetit <pablopetit94@gmail.com>
2014-11-14 17:48:49 +0100

    zoom qui marche

--- src/GUI/ViewPort.java
+++ src/GUI/ViewPort.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import static GUI.MouseListenerPanel.inMoove;
 import moutonsimulator.Config;
 import moutonsimulator.Jeu.ConfigInitial;
 
@@ -16,29 +17,42 @@ public class ViewPort {
     public static void resize(int width, int height) {
         if (ViewPort.x + width > ViewPort.widthMax) {
             if (width > ViewPort.widthMax) {
-                ViewPort.width=ViewPort.widthMax;
-                ViewPort.x=0;
+                ViewPort.width = ViewPort.widthMax;
+                ViewPort.x = 0;
             } else {
                 int xTmp = ViewPort.widthMax - width;
                 System.out.println(xTmp);
                 ViewPort.width = width;
                 ViewPort.x = xTmp;
             }
-        }else{
-            ViewPort.width=width;
+        } else {
+            ViewPort.width = width;
         }
         if (ViewPort.y + height > ViewPort.heightMax) {
             if (height > ViewPort.heightMax) {
-                ViewPort.height=ViewPort.heightMax;
-                ViewPort.y=0;
+                ViewPort.height = ViewPort.heightMax;
+                ViewPort.y = 0;
             } else {
                 int yTmp = ViewPort.heightMax - height;
                 ViewPort.height = height;
                 ViewPort.y = yTmp;
             }
-        }else{
-            ViewPort.height=height;
+        } else {
+            ViewPort.height = height;
         }
         ViewPort.panel.repaint();
+    }
+
+    public static void zoom(int wheel) {
+        int nC = Config.coteCase - 2 * wheel;
+        if (nC >= 16 && nC <= 64 && !inMoove) {
+            ViewPort.x = (ViewPort.x / Config.coteCase) * nC;
+            ViewPort.y = (ViewPort.y / Config.coteCase) * nC;
+            ViewPort.heightMax = (nC * ConfigInitial.heigth);
+            ViewPort.widthMax = (nC * ConfigInitial.width);
+            Config.coteCase = nC;
+            ViewPort.panel.repaint();
+        }
+
     }
 }
