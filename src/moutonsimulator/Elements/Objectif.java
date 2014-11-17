c68e4abc1f56d7af0b307ec3d34718ee008fe4f8
PabloPetit <pablopetit94@gmail.com>
2014-11-17 13:20:48 +0100

    objectif

---
+++ src/moutonsimulator/Elements/Objectif.java
@@ -0,0 +1,19 @@
+package moutonsimulator.Elements;
+
+import moutonsimulator.Jeu.Case;
+
+public class Objectif {
+    
+    int point;
+    Case propriaitaire;
+    Case cible;
+    
+    public Objectif(Case prop,Case cible){
+        this.propriaitaire = prop;
+        this.cible = cible;
+        this.point = 0;
+    }
+    
+    public void ev
+    
+}
