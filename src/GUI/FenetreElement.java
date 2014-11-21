3f431a6d870739746a5ecd5e70d671b9d3122cca
luCmoi <guillaume.bertrand@etu.univ-paris-diderot.fr>
2014-11-21 16:22:53 +0100

    Un peu de mise en place

--- src/GUI/FenetreElement.java
+++ src/GUI/FenetreElement.java
@@ -6,7 +6,6 @@ import java.awt.Insets;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import moutonsimulator.Elements.Animal;
 
 public class FenetreElement extends JFrame {
@@ -24,42 +23,40 @@ public class FenetreElement extends JFrame {
         this.setSize(400,200);
         this.setResizable(false);
         
-        JLabel image = new JLabel(new ImageIcon(info.getImage().getScaledInstance(32, 32, java.awt.Image.SCALE_SMOOTH)));
+        JLabel image = new JLabel(new ImageIcon(info.getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH)));
         contraintes.gridx = 0;
         contraintes.gridy = 0;
         contraintes.gridwidth = 2;
         contraintes.gridheight = 2;
-        //contraintes.weightx = 1;
-        //contraintes.weighty = 1;
-        this.add(image);
+        this.add(image,contraintes);
 
         JLabel age = new JLabel("Age : " + info.getCompetence().getAge().getVal());
-        contraintes.gridx = 1;
+        contraintes.gridx = 2;
         contraintes.gridy = 0;
         contraintes.gridwidth = 1;
         contraintes.gridheight = 1;
-        this.add(age);
+        this.add(age,contraintes);
         
         JLabel vie = new JLabel("Vie : " + info.getCompetence().getVie().getVal()+"/"+info.getCompetence().getVie().getMax());
-        contraintes.gridx = 1;
+        contraintes.gridx = 2;
         contraintes.gridy = 1;
         contraintes.gridwidth = 1;
         contraintes.gridheight = 1;
-        this.add(vie);
+        this.add(vie,contraintes);
         
         JLabel vue = new JLabel("Vue : " + info.getCompetence().getVue());
-        contraintes.gridx = 2;
+        contraintes.gridx = 3;
         contraintes.gridy = 0;
         contraintes.gridwidth = 1;
         contraintes.gridheight = 1;
-        this.add(vue);
+        this.add(vue,contraintes);
         
         JLabel puissance = new JLabel("Puissance : " + info.getCompetence().getPuissance());
-        contraintes.gridx = 2;
+        contraintes.gridx = 3;
         contraintes.gridy = 1;
         contraintes.gridwidth = 1;
         contraintes.gridheight = 1;
-        this.add(puissance);
+        this.add(puissance,contraintes);
         this.setVisible(true);
     }
 }
