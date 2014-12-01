ee95bdb0424ff0eef57696bff4f6be6d259ce4a1
PabloPetit <pablopetit94@gmail.com>
2014-12-01 13:45:03 +0100

    ondcoun

--- src/moutonsimulator/Jeu/ConfigInitial.java
+++ src/moutonsimulator/Jeu/ConfigInitial.java
@@ -7,9 +7,9 @@ public class ConfigInitial {
     public int nbMouton = 0;
     public int nbLoup = 0 ;
     private int probaPlante = 0;
-    public int nbFamillePlante = 200;
+    public int nbFamillePlante = 0;
+    private int eau = 40000;
     private int nbMaxFamille;
-    private int eau = 0;
     
 
     public int getWidth() {
