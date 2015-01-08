b8585ba8a79b975bdb421a4f23c5b613f3a42c7c
luCmoi <guillaume.bertrand@etu.univ-paris-diderot.fr>
2015-01-08 13:32:42 +0100

    yop

--- src/moutonsimulator/Elements/PathFinding.java
+++ src/moutonsimulator/Elements/PathFinding.java
@@ -4,12 +4,30 @@ import java.awt.Point;
 import java.util.HashMap;
 import moutonsimulator.Jeu.Case;
 
+/**
+ * Class de calcul afin de réaliser le pathfinding avec l'algorithme Astar
+ */
 public class PathFinding {
 
+    /**
+     * Retourne la distance entre deux points
+     * @param a
+     * @param b
+     * @return 
+     */
     public static double distance(Point a, Point b) {
         return Math.sqrt(((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y)));
     }
 
+    /**
+     * Ajoute les cases voisines d'une case passe et les traites
+     * @param p
+     * @param destination
+     * @param listeOuverte
+     * @param listeFermee
+     * @param plateau
+     * @param vue 
+     */
     public static void ajoutCaseVoisines(Point p, Point destination, HashMap<Point, Noeud> listeOuverte, HashMap<Point, Noeud> listeFermee, Case[][] plateau, int vue) {
         Noeud tmp = new Noeud();
         for (int i = Math.max(0, p.x - 1); i < Math.min(p.x + 2, plateau.length); i++) {
@@ -39,11 +57,22 @@ public class PathFinding {
         }
     }
 
+    /**
+     * Déplace un élément de la listeOuverte vers la listeFermee
+     * @param p
+     * @param listeFermee
+     * @param listeOuverte 
+     */
     public static void ajoutListeFermee(Point p, HashMap<Point, Noeud> listeFermee, HashMap<Point, Noeud> listeOuverte) {
         listeFermee.put(p, listeOuverte.get(p));
         listeOuverte.remove(p);
     }
 
+    /**
+     * Retourne le meilleur noeud a traiter parmis la liste
+     * @param listeOuverte
+     * @return 
+     */
     public static Point meilleurNoeud(HashMap<Point, Noeud> listeOuverte) {
         float m_coutf = -1;
         Point m_noeud = new Point();
@@ -59,6 +88,14 @@ public class PathFinding {
         return m_noeud;
     }
 
+    /**
+     * Parcour les résultat obtenus pour savoir sur quelle case aller
+     * @param destination
+     * @param depart
+     * @param listeFermee
+     * @param but
+     * @return 
+     */
     public static Point retrouver_chemin(Point destination, Point depart, HashMap<Point, Noeud> listeFermee, Objectif but) {
         Noeud tmp = listeFermee.get(destination);
         Point precedent = new Point();
